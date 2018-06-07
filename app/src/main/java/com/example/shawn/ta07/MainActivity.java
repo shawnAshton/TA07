608773c17becb77898985f07bca610052c56e242
shawnAshton <ash14009@byui.edu>
2018-06-06 22:15:03 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/shawn/ta07/MainActivity.java

--- app/src/main/java/com/example/shawn/ta07/MainActivity.java
+++ app/src/main/java/com/example/shawn/ta07/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Testing testing Shawn !!!!!!!!!!!!is here testing goober
-        //HI SHAWN!!!          3.0
+        //BECAUSE
 
     }
 }
