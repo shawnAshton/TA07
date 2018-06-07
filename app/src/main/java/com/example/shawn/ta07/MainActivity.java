e2108a5683f774134a041e762435e51f08e3be7c
Justin Bengtson <justinbengtson@icloud.com>
2018-06-06 22:11:57 -0600

    Testing git things 3

--- app/src/main/java/com/example/shawn/ta07/MainActivity.java
+++ app/src/main/java/com/example/shawn/ta07/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // Shawn WHY?????????????
         // Testing testing Shawn !!!!!!!!!!!!is here testing goober
         //HI SHAWN!!!          3.0
 
